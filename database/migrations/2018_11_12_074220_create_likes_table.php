618e56ecdbb25a12fcab173401357576b6cda674
Jimwell Ramirez <jcramirez2143@gmail.com>
2018-11-12 16:15:23 +0800

    Creating Migration, Model, Controller, Factories

--- database/migrations/2018_11_12_074220_create_likes_table.php
+++ database/migrations/2018_11_12_074220_create_likes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRepliesTable extends Migration
+class CreateLikesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateRepliesTable extends Migration
      */
     public function up()
     {
-        Schema::create('replies', function (Blueprint $table) {
+        Schema::create('likes', function (Blueprint $table) {
             $table->increments('id');
-            $table->text('body');
-
-            $table->integer('question_id')->unsigned();
+            $table->integer('reply_id');
             $table->integer('user_id');
-
-            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateRepliesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('replies');
+        Schema::dropIfExists('likes');
     }
 }
